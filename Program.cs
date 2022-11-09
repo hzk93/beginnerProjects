12b041bb598fd86a07e700418ed2e8673b88ba31
ZahooHas <ZahooHas@CRKRW-MVC7>
2022-11-09 14:51:12 +0500

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿namespace beginnerProjects
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello this is the main calling method below are all the classes\n");
+            mathsTable mathstable = new mathsTable();
+            mathstable.myTable();
+            
+        }
+    }
+}
---
+++ mathsTable.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace beginnerProjects
+{
+    public class mathsTable
+    {
+        public void myTable()
+        {
+            int a = 2;
+            for (int i=1; i<=20; i++)
+            {
+                Console.WriteLine($"{a} x {i} = {a * i}");
+            }
+        }
+    }
+}
